5a8bd74a8b7eab42c35bd4e98283af27feb0e75a
KirillGos <106958670+KirillGos@users.noreply.github.com>
2022-08-05 23:35:13 +0300

    Made a few changes

--- script.js
+++ script.js
@@ -1,59 +1,32 @@
-// Variables
-//RESET
-const reset = document.querySelector('.reset');
-// output 
-const output = document.querySelector('.output');
-//operators
-const addBtn = document.querySelector('.addBtn');
-const subtractBtn = document.querySelector('.subtractBtn');
-const multiplyBtn = document.querySelector('.multiplyBtn');
-const divideBtn = document.querySelector('.divideBtn');
+// variables 
 
-//numbers
-const numbers = document.querySelector('.numbers');
-// basic math operators 
-const add = (a, b) => {
-    return a + b;
+const buttons = Array.from(document.querySelectorAll('button'));
+const output = document.querySelector('.display');
+// Basic math operators
+
+const add = (a, b) => a + b;
+const subtract = (a, b) => a - b;
+const multiply = (a, b) => a * b;
+const divide = (a, b) => a / b;
+
+// operate function 
+function operate(operator,a , b) {
+    if(operator === 'add') {
+        return   add(a, b); 
+    } else if (operator === 'subtract') {
+        return  subtract(a, b);
+    } else if(operator === 'divide') {
+        return divide(a, b);
+    } else if(operator === 'multiply') {
+        return multiply(a, b); 
+    } 
 }
 
-const subtract = (a, b) => {
-    return a - b;
-} 
+// display
 
-const multiply = (a, b) => {
-    return a * b;
+function population() {
+    buttons.map(button => button.addEventListener('click', (e) => {
+        output.textContent += e.target.textContent  ;
+    }))
 }
-
-const divide = (a, b) => {
-    return a / b;
-}
-
-
-let display;
-document.addEventListener('click', function(e) {
-    if(e.target.className.includes('num')) {
-        output.innerText += e.target.innerText; 
-        display = output.innerText;
-     }
-});
-let operation;
-document.addEventListener('click', function(e) {
-    if(e.target.parentElement.className == 'operators') {
-        operation = e.target.className;
-        console.log(operation)
-        }
-});
-//reset button
-reset.addEventListener('click', function(e) {
-    output.innerText = '';
-});
-
-// addBtn.addEventListener('click', function() {
-//     let first = display;
-//    let second = output.innerText; 
-//   output.innerText =  add(parseInt(first), parseInt(second));
-// })
-
-
-//operate function
-
+population()
